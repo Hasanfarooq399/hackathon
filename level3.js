2fb62572b293df119ce95744d3d06b836888289d
Hasanfarooq399 <90978017+Hasanfarooq399@users.noreply.github.com>
2021-09-19 19:06:33 +0500

    Add files via upload

---
+++ level3.js
@@ -0,0 +1,33 @@
+let lives =3
+let score=0
+let level=3;
+let bluecount=1;
+let usercount=1;
+for (let index = 0; index <40; index++) {
+    let nodeCreate=document.createElement("div");
+    nodeCreate.className="baloon"
+    nodeCreate.id="baloon"+index;
+
+    nodeCreate.setAttribute("onmouseover","pop_baloon(baloon"+index+")")
+    let colour=Math.ceil(Math.random() * 4)
+
+    if(colour===1){
+        nodeCreate.style.backgroundColor="red"
+    }
+    else if(colour===2){
+        nodeCreate.style.backgroundColor="blue"
+        bluecount++;
+    }
+    else if(colour===3){
+        nodeCreate.style.backgroundColor="green"
+    }
+    else {
+        nodeCreate.style.backgroundColor="yellow"
+    }
+    
+    document.getElementById("platform").appendChild(nodeCreate);
+
+}
+
+function pop_baloon(baloonNumber);
+    balooncolour="blue"
